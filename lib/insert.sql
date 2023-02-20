902ad806acb36f863569cb7a31ffbf9b50283898
e-nk <enocklagat747@gmail.com>
2023-02-20 19:59:39 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+("Mr. Chocolate", 25, "M", "Gray", "quiet", false),
+("Rowdy", 8, "F", "White", "noisy", true),
+("Tabitha", 12, "M", "Brown", "calm", false),
+("Sergeant Brown", 32, "F", "Gray", "aggressive", false),
+("Melissa", 16, "F", "Dark Gray", "friendly", true),
+("Grinch", 48, "M", "Pink", "sweet", false),
+("Wendy", 23, "F", "Gray", "choleric", true),
+(NULL, 27, "M", "Blue", "calm", false);
